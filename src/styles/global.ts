1e077e568093bd2967561e8c73d314254d1d6001
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-12-18 00:23:03 -0300

    Add css variables in GlobalStyles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,21 +1,44 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
 
-    html, body, #__next {
-        height: 100%;
-    }
+  :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eeeeee;
+    --grey: #cccccc;
+    --container: 100rem;
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
 
-    html {
-        font-size: 62.5%;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    body {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-    }
+  html, body, #__next {
+    height: 100%;
+    background-color: var(--background);
+    color: var(--white);
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p, a {
+    font-size: 2rem;
+    line-height: var(--medium)
+  }
+
+  a {
+    color: var(--highlight);
+  }
 `;
