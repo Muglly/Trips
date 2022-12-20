fcbe268315e66feb1a9f6f13e049546bc81b5d93
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-12-20 11:32:38 -0300

    Changed page About for Slog, add GetStaticProps method and autogenerated generic pages

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -3,22 +3,22 @@ import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline';
 
 import * as S from './styles';
 
-export default function PagesTemplate() {
+export type PageTemplateProps = {
+  heading: string;
+  body: string;
+};
+
+export default function PageTemplate({ heading, body }: PageTemplateProps) {
   return (
     <S.Content>
       <LinkWrapper href="/">
         <CloseOutline size={32} />
       </LinkWrapper>
 
-      <S.Heading>My Trips</S.Heading>
+      <S.Heading>{heading}</S.Heading>
 
       <S.Body>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
-          cupiditate magni excepturi, eum corrupti consequuntur quisquam cumque
-          reiciendis officia qui fuga labore dolorum suscipit velit nemo
-          recusandae earum minus aut.
-        </p>
+        <div dangerouslySetInnerHTML={{ __html: body }} />
       </S.Body>
     </S.Content>
   );
