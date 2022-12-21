97088af5ac2046692ab2a664c4ec511d00b38fa6
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-12-21 11:08:03 -0300

    Add query Places and getStaticProps in page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
 import { MapProps } from 'components/Map';
 import HomeTemplate from 'templates/Home';
 
+import { GetPlacesQuery } from 'graphql/generated/graphql';
+import { GET_PLACES } from 'graphql/queries';
+import client from 'graphql/client';
+
 export default function Home({ places }: MapProps) {
   return <HomeTemplate places={places} />;
 }
+
+export const getStaticProps = async () => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES);
+
+  return {
+    props: {
+      places,
+    },
+  };
+};
